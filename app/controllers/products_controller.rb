2e160be44b113a2bd1c4da13c9d661e85c3c8db0
Sam Wells <sfwells0518@gmail.com>
2023-03-29 15:20:11 -0500

    add new all products method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,26 @@
 class ProductsController < ApplicationController
   def show_ipad
-    # @products = product.find_by(id: 1)
+    # @product = Product.find_by(id: 1)
     # render template: "products/show"
+
     render json: { message: "hello" }
   end
 
   def show_macbook
-    # @products = product.find_by(id: 2)
-    # render template: "products/show"
-    render json: { message: "hello" }
+    @product = Product.find_by(id: 2)
+    render template: "products/show"
+    # render json: { message: "hello" }
   end
 
   def show_iphone_14_pro
-    # @products = product.find_by(id: 3)
-    # render template: "products/show"
-    render json: { message: "hello" }
+    @product = Product.find_by(id: 3)
+    render template: "products/show"
+    # render json: { message: "hello" }
+  end
+
+  def show_products_all
+    @products = Product.all
+    render template: "products/index"
+    # render json: { message: "hello" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/ipad_pro", controller: "products", action: "show_ipad"
   get "/macbook_pro", controller: "products", action: "show_macbook"
   get "/iphone_14_pro", controller: "products", action: "show_iphone_14_pro"
+  get "/products_all", controller: "products", action: "show_products_all"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
