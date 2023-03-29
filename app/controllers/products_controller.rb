530824d405d899c0311641cceb72f3ed8801e2f8
Sam Wells <sfwells0518@gmail.com>
2023-03-29 14:13:06 -0500

    create get routes for ipad, macbook, iphone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,7 @@
 class ProductsController < ApplicationController
+  def show_ipad
+    @contact = product.find_by(id: 1)
+    render template: "contacts/show"
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/ipad_pro", controller: "products", action: "show_ipad"
+  get "/macbook_pro", controller: "products", action: "show_macbook"
+  get "/iphone_14_pro", controller: "products", action: "show_iphone_14_pro"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
