5fac882b8aa6c3441389fe498b1867a32148d9e6
Sam Wells <sfwells0518@gmail.com>
2023-03-29 14:22:45 -0500

    add products to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
   def show_ipad
-    @contact = product.find_by(id: 1)
-    render template: "contacts/show"
+    # @products = product.find_by(id: 1)
+    # render template: "products/show"
+    render json: { message: "hello" }
+  end
 
+  def show_macbook
+    # @products = product.find_by(id: 2)
+    # render template: "products/show"
+    render json: { message: "hello" }
+  end
 
+  def show_iphone_14_pro
+    # @products = product.find_by(id: 3)
+    # render template: "products/show"
+    render json: { message: "hello" }
+  end
 end
