54574d1ae821353113789e24c1c8e339563b0ac3
Sam Wells <sfwells0518@gmail.com>
2023-03-30 11:37:21 -0500

    remove routes and controller code in prep for new, correct conventions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,4 @@
 class ProductsController < ApplicationController
-  def show_ipad
-    # @product = Product.find_by(id: 1)
-    # render template: "products/show"
-
-    render json: { message: "hello" }
-  end
-
-  def show_macbook
-    @product = Product.find_by(id: 2)
-    render template: "products/show"
-    # render json: { message: "hello" }
-  end
-
-  def show_iphone_14_pro
-    @product = Product.find_by(id: 3)
-    render template: "products/show"
-    # render json: { message: "hello" }
-  end
-
-  def show_products_all
-    @products = Product.all
-    render template: "products/index"
-    # render json: { message: "hello" }
-  end
+  def 
+  end 
 end
