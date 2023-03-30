d3c9af4d28729e5daff45d61c6efca839dfe8d5a
Sam Wells <sfwells0518@gmail.com>
2023-03-30 12:32:38 -0500

    add created at and updated at to products jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render :show
+  end
 end
