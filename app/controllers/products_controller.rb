cfe329270d5f43f2fa0e1d3776e8088d6813c53c
Sam Wells <sfwells0518@gmail.com>
2023-03-30 13:10:07 -0500

    make create action in route and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    # make new product in the db
+    @product = Product.new(name: "Apple Pencil", price: 75, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd
+      9GcScglgtPfXO1pSHCjKHH5dKtBW0CzokHxbnG2Hz7Ms-DA&usqp=CAUec=48600112", description: "Apple Pencil expands the power of iPad and opens up new creative possibilities.")
+    @product.save
+    render :show
+    # copy/paste that code from rails console here
+    # then @product.save
+    # finally, render :show
+  end
 end
