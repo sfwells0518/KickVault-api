8fa7e0ba9fbe4ddb387dca66492b11202df6d876
Sam Wells <sfwells0518@gmail.com>
2023-03-31 11:18:19 -0500

    use default values with empty params on update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,10 +29,10 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.price = params[:price]
-    @product.image_url = params[:image_url]
-    @product.description = params[:description]
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
     # save updated product
     @product.save
     render :show
