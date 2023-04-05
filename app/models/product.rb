52bb00498ee428e6cdd2902cac045b6d038d8472
Sam Wells <sfwells0518@gmail.com>
2023-04-05 11:54:47 -0500

    add validations for price numericality and description presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true 
   validates :description, length: { maximum: 500 }
 
   def is_discounted?
