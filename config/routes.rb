487495b81c3201317b1d94c8c5d359b29da00a7b
Sam Wells <sfwells0518@gmail.com>
2023-04-17 11:36:41 -0500

    add post REST route for users create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # Product Routes:
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  # User Routes:
+  post "/users" => "users#create"
 end
